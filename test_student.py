b308cfa8627e9892bbddc129f34069f4368b4db5
hartnetl <laura.hartnett@outlook.com>
2021-09-28 12:54:19 +0000

    Lesson 2: Create test for alert santa

--- test_student.py
+++ test_student.py
@@ -3,11 +3,20 @@ from student import Student
 
 
 class TestStudent(unittest.TestCase):
-    # Test the full name
+    # Test the full name function
     def test_full_name(self):
         # We need to create an instance to test it
         student = Student("John", "Doe")
         self.assertEqual(student.full_name, "John Doe")
+
+    # Create test for changing naughty list to True
+    def test_alert_santa(self):
+        # Create instance
+        student = Student("John",  "Doe")
+        # Call the function
+        student.alert_santa()
+        # Check it changed
+        self.assertTrue(student.naughty_list)
 
 
 if __name__ == "__main__":
